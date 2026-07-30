38cc0509fa7a8e6d7feffbf798c292297ce1ad97
degino-dev <walteremanuelb@gmail.com>
2026-07-30 09:08:41 -0300

    Corrige parametros TLS do MongoDB

--- admin/server.js
+++ admin/server.js
@@ -10,14 +10,19 @@ const ADMIN_KEY = "pncpAdmin2026!";
 // =========================================================
 // CONEXÃO MONGODB ATLAS
 // =========================================================
-const MONGO_URI = process.env.MONGO_URI || "mongodb://walteremanuel_db_user:oEY8KMaKvhIZEljT@ac-lw4fvfw-shard-00-00.fsea6qz.mongodb.net:27017,ac-lw4fvfw-shard-00-01.fsea6qz.mongodb.net:27017,ac-lw4fvfw-shard-00-02.fsea6qz.mongodb.net:27017/pncp_monitor?ssl=true&sslInvalidHostNameAllowed=true&replicaSet=atlas-k8u0k2-shard-0&retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://walteremanuel_db_user:oEY8KMaKvhIZEljT@cluster0.fsea6qz.mongodb.net/pncp_monitor?retryWrites=true&w=majority";
 const DB_NAME = "pncp_monitor";
 
 let db;
 let client;
 
 async function conectarMongo() {
-  client = new MongoClient(MONGO_URI);
+  client = new MongoClient(MONGO_URI, {
+    tls: true,
+    tlsAllowInvalidCertificates: true,
+    tlsAllowInvalidHostnames: true,
+    serverSelectionTimeoutMS: 10000
+  });
   
   await client.connect();
   db = client.db(DB_NAME);
